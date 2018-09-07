518575901f803dd02443db3524b318a67c958039
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-09-07 15:23:07 -0400

    done

--- scripts.js
+++ scripts.js
@@ -6,13 +6,7 @@ let images = [
     "http://homework.warbyparker.com/images/kerouac-orange.jpg",
     "http://homework.warbyparker.com/images/kerouac-red.jpg"
 ];
-
-//add overlay image to the DOM
-function handleOverlayImage(e, element, index, overlay) {
-    if (e.target.id.substr(-1) === index.toString()) {  
-        overlay.innerHTML = "<img id=\"overlay-image\" src='" + element + "'>";
-    }
-}
+let i = 0;
 
 images.forEach((element, index) => {
     let overlay = document.getElementById("overlay");
@@ -24,25 +18,31 @@ images.forEach((element, index) => {
     thumbnail.src = element;
     thumbnail.setAttribute("id", "thumbnail-" + index);
     thumbnailContainer.appendChild(thumbnail);
+});
 
-    window.addEventListener("click",  (e) =>  { handleOverlayImage(e, element, index, overlay); });
-    
+//lightbox controls
+window.addEventListener("click", (e) => {
+    if (e.target.id == "backward") {
+        i = i + images.length;
+        document.getElementById("overlay-image").src = images[i-- % images.length];
+    } else if (e.target.id == "forward") {
+        document.getElementById("overlay-image").src = images[i++ % images.length];
+    }
 });
 
 //on scroll, if the user is at the top of the page, lightbox container = 80%. if at the bottom, lightbox container = one-third the original width of the container. 
 //otherwise, decrease container width as user scrolls down.
-function changeWidth(lightboxContainer, computedWidth) {
-    lightboxContainer.style.width = (computedWidth - window.pageYOffset).toString() + "px";
-}
-
-window.addEventListener("scroll",  () => { 
+window.addEventListener("scroll", () => {
     let lightboxContainer = document.getElementById("container");
     let computedWidth = parseInt(getComputedStyle(lightboxContainer).getPropertyValue("width").slice(0, -2));
-    if (document.documentElement.scrollTop === 0 ) {
-        lightboxContainer.style.width = "80%";   
+    if (document.documentElement.scrollTop === 0) {
+        lightboxContainer.style.width = "80%";
     } else if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
         lightboxContainer.style.width = "calc(80% /3)";
     } else {
-        changeWidth(lightboxContainer, computedWidth);
+        lightboxContainer.style.width = (computedWidth - window.pageYOffset).toString() + "px";
     }
-}); 
+});
+
+
+
